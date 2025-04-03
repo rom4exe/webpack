df594e1c91e6f9ed17bd66312efa8b0deed53339
Rom4exe <rom.exe@mail.ru>
2025-04-03 13:31:15 +0800

    edit_dz

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
+import Game, {
+  GameSavingData,
+  readGameSaving as loadGame,
+  writeGameSaving as saveGame,
+} from "./game.js";
+
+const game = new Game();
+game.start();
 console.log("app worked");
---
+++ src/js/game.js
@@ -0,0 +1,21 @@
+import { Character } from "./domain.js";
+
+export { readGameSaving, writeGameSaving };
+
+export default class Game {
+  start() {
+    console.log("game started");
+  }
+}
+
+export class GameSavingData {
+
+}
+
+export function readGameSaving() {
+
+}
+
+export function writeGameSaving() {
+
+}
